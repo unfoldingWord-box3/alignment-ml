fcee48860d333f87fb3dc40c5ca46e6598a3c133
Bruce McLean <mcleanb@photo-nomad.net>
2020-12-04 05:50:08 -0500

    database fun

---
+++ demo.py
@@ -0,0 +1,31 @@
+
+import utils.bible_utils as bu
+
+userUrl = 'https://git.door43.org/lrsallee'
+bibleType = 'en_ult'
+testament = 1
+outputFolder = './data'
+
+bu.downloadTestamentAlignments(userUrl, bibleType, testament, outputFolder)
+
+################################
+
+import utils.db_utils as db
+
+connection = db.create_connection('./data/dummy.sqlite')
+
+create_users_table = """
+CREATE TABLE IF NOT EXISTS word (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  book_id TEXT NOT NULL,
+  chapter TEXT NOT NULL,
+  verse TEXT NOT NULL,
+  word_num INTEGER,
+  word TEXT NOT NULL,
+  occurrance INTEGER,
+  occurrances INTEGER,
+  strong TEXT,
+  lemma TEXT,
+  morph TEXT
+);
+"""
--- dowload_testament.py
+++
@@ -1,9 +0,0 @@
-
-import utils.bible_utils
-
-userUrl = 'https://git.door43.org/lrsallee'
-bibleType = 'en_ult'
-testament = 1
-outputFolder = './data'
-
-utils.bible_utils.downloadTestamentAlignments(userUrl, bibleType, testament, outputFolder)
